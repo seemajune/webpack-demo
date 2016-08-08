285c94aba22bcff80253f01fda9e278bfbc08c1e
Seema Shariat <sshariat@750-MAC-AFVH8.local>
2016-08-08 10:55:54 -0400

    adds hmr for css

--- webpack.config.js
+++ webpack.config.js
@@ -33,10 +33,14 @@ var config;
 // detect how npm is run and branch based on that 
 switch(process.env.npm_lifecycle_event) {
   case "build":
-    config = merge(common, {});
+    config = merge(
+      common, parts.setupCSS(PATHS.app)
+    );
     break;
   default: 
-    config = merge(common, parts.devServer({
+    config = merge(common, 
+      parts.setupCSS(PATHS.app),
+      parts.devServer({
       //customize host/port here if needed
       host: process.env.HOST,
       port: process.env.PORT
